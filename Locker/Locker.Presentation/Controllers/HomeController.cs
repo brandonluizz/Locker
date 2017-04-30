7d6ed78b8ca784db43d6e45100afbaf7768347d7
Unknown <brandonluizz@hotmail.com>
2017-04-30 16:57:16 -0300

    remove class Teste in homeController

--- Locker/Locker.Presentation/Controllers/HomeController.cs
+++ Locker/Locker.Presentation/Controllers/HomeController.cs
@@ -1,25 +1,16 @@
-﻿using Locker.Application;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System;
 using System.Web.Mvc;
 
 namespace Locker.Presentation.Controllers
 {
     public class HomeController : Controller
     {
-        private ITeste teste;
-
-        public HomeController(ITeste teste)
+        public HomeController()
         {
-            this.teste = teste ?? throw new ArgumentNullException(nameof(teste));
         }
 
         public ActionResult Index()
         {
-            teste.AddNewUser();
-
             return View();
         }
 
