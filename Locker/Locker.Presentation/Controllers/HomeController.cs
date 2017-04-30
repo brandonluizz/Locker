5a5185193c150e2b5dbf7876284ae296c13a96cc
Unknown <brandonluizz@hotmail.com>
2017-04-30 17:52:25 -0300

    update HomeController

--- Locker/Locker.Presentation/Controllers/HomeController.cs
+++ Locker/Locker.Presentation/Controllers/HomeController.cs
@@ -1,31 +1,15 @@
-﻿using System;
+﻿using Locker.DomainModel;
+using Locker.Infrastructure.Repositories.Interface;
+using System;
 using System.Web.Mvc;
 
 namespace Locker.Presentation.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
-        {
-        }
-
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        }        
     }
 }
