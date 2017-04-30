1d56bdee760f10aa7d1164fe6bcb7dfdf67a0865
Unknown <brandonluizz@hotmail.com>
2017-04-30 16:56:25 -0300

    remove UnityCOnfig in Global.asax

--- Locker/Locker.Presentation/Global.asax.cs
+++ Locker/Locker.Presentation/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using Locker.Presentation.App_Start;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -17,7 +12,6 @@ namespace Locker.Presentation
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            UnityConfig.RegisterComponents();
         }
     }
 }
